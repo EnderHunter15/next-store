1dce37153994b1201bb3c669becd5e9ce7fe17b4
EnderHunter15 <coatudinu15@gmail.com>
2025-03-15 22:35:06 +0200

    admin crud functionality and favorites functionality fix

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -7,33 +7,32 @@ import { SubmitButton } from '@/components/form/Buttons';
 import CheckboxInput from '@/components/form/CheckboxInput';
 import ImageInputContainer from '@/components/form/ImageInputContainer';
 
-interface PageProps {
-  params: { id: string };
-}
-
-export default async function EditProductPage({ params }: PageProps) {
-  const { id } = params;
-  const product = await fetchAdminProductDetails(id);
-  const { name, company, description, featured, price } = product;
+export default async function EditProductPage({ params }: { params: { id: string } }) {
+  const product = await fetchAdminProductDetails(params.id);
 
   return (
     <section>
       <h1 className='text-2xl font-semibold mb-8 capitalize'>update product</h1>
       <div className='border p-8 rounded'>
-        <ImageInputContainer action={updateProductImageAction} name={name} image={product.image} text='update image'>
-          <input type='hidden' name='id' value={id} />
+        <ImageInputContainer
+          action={updateProductImageAction}
+          name={product.name}
+          image={product.image}
+          text='update image'
+        >
+          <input type='hidden' name='id' value={params.id} />
           <input type='hidden' name='url' value={product.image} />
         </ImageInputContainer>
         <FormContainer action={updateProductAction}>
           <div className='grid gap-4 md:grid-cols-2 my-4'>
-            <input type='hidden' name='id' value={id} />
-            <FormInput type='text' name='name' label='product name' defaultValue={name} />
-            <FormInput type='text' name='company' defaultValue={company} />
-            <PriceInput defaultValue={price} />
+            <input type='hidden' name='id' value={params.id} />
+            <FormInput type='text' name='name' label='product name' defaultValue={product.name} />
+            <FormInput type='text' name='company' defaultValue={product.company} />
+            <PriceInput defaultValue={product.price} />
           </div>
-          <TextAreaInput name='description' labelText='product description' defaultValue={description} />
+          <TextAreaInput name='description' labelText='product description' defaultValue={product.description} />
           <div className='mt-6'>
-            <CheckboxInput name='featured' label='featured' defaultChecked={featured} />
+            <CheckboxInput name='featured' label='featured' defaultChecked={product.featured} />
           </div>
           <SubmitButton text='update product' className='mt-8' />
         </FormContainer>
